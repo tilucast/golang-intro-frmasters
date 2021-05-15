dd16a6f02035e8ff3d91cb70fcd139edc0f06224
Lucas <tilucast@gmail.com>
2021-05-14 21:50:02 -0300

    go fr masters initial commit

---
+++ 02_introduction/code/exercise2.go
@@ -0,0 +1,9 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Print("Hello world.")
+	fmt.Println("Hello world.")
+	fmt.Printf("Hello my name is %s, I have lived in %s for basically my entire life, they say the weather is nice, which is %t", "Lucas", "Brazil", false)
+}
---
+++ test/structure.go
@@ -0,0 +1,7 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Printf("Hello my name is %s", "Lucas")
+}
