db05c769d369e3435f9b02f5bd4e24305c428916
Lucas <tilucast@gmail.com>
2021-05-27 14:06:04 -0300

    cap 11 server rendering with go, almost finished

--- 11_server/code/server.go
+++ 11_server/code/server.go
@@ -1,7 +1,47 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"text/template"
+)
+
+func home(w http.ResponseWriter, r * http.Request){
+	fmt.Println("I am home boi")
+}
+
+type PageData  struct{
+	PageTitle string
+	PageTodos []Todo
+}
+
+type Todo struct{
+	Title string
+	Content string
+}
+
+var todos []Todo
+
+func getTodos(w http.ResponseWriter, r * http.Request){
+	pageData := PageData{
+		PageTitle: "Get todos",
+		PageTodos: todos,
+	}
+
+	t, err := template.ParseFiles("todos.html")
+
+	if err != nil{
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		log.Print("template parsing error: ", err)
+	}
+
+	err = t.Execute(w, pageData)
+}
 
 func main() {
-	fmt.Println("Building a web server")
+	http.HandleFunc("/", home)
+	http.HandleFunc("/todos", getTodos)
+	fmt.Println("Server on port :8080")
+	log.Fatal(http.ListenAndServe(":8080", nil))
 }
