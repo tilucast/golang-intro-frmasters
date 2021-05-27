0c9ac0e767ecc4e6dbf50262bb3ae861cdbeea2a
Lucas <tilucast@gmail.com>
2021-05-27 10:02:48 -0300

    cap 9

--- 09_methods/code/methods.go
+++ 09_methods/code/methods.go
@@ -2,20 +2,34 @@ package main
 
 import "fmt"
 
-// User is a user type
 type User struct {
 	ID                         int
 	FirstName, LastName, Email string
 }
 
-func describeUser(u User) string {
-	desc := fmt.Sprintf("Name: %s %s, Email: %s", u.FirstName, u.LastName, u.Email)
-	return desc
+type Group struct {
+	role string
+	users []User
+	newestUser User
+	spaceAvailable bool
+}
+
+func (group *Group) describe(){
+	
+	group.spaceAvailable = false
+	fmt.Println(group)
+	
+}
+
+func (user User) describe() {
+	fmt.Printf("Name: %s %s, Email: %s \n", user.FirstName, user.LastName, user.Email)
 }
 
 func main() {
 	user := User{ID: 1, FirstName: "Marilyn", LastName: "Monroe", Email: "marilyn.monroe@gmail.com"}
 
-	desc := describeUser(user)
-	fmt.Println(desc)
+	group := Group{role: "Janitors", users: []User{user}, newestUser: user, spaceAvailable: true}
+
+	user.describe()
+	group.describe()
 }
