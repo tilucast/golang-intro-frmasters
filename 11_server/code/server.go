6d293a9a11dd23747d9d5659ad230c483b3f9148
Lucas <tilucast@gmail.com>
2021-05-27 16:36:19 -0300

    server side rendering cap 11

--- 11_server/code/server.go
+++ 11_server/code/server.go
@@ -8,7 +8,7 @@ import (
 )
 
 func home(w http.ResponseWriter, r * http.Request){
-	fmt.Println("I am home boi")
+	//fmt.Println("I am home boi")
 }
 
 type PageData  struct{
@@ -39,9 +39,27 @@ func getTodos(w http.ResponseWriter, r * http.Request){
 	err = t.Execute(w, pageData)
 }
 
+func AddTodo(w http.ResponseWriter, r * http.Request){
+	err := r.ParseForm()
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		log.Print("Request parsing error: ", err)
+	}
+
+	todo := Todo{
+		Title: r.FormValue("title"),
+		Content: r.FormValue("content"),
+	}
+
+	todos = append(todos, todo)
+	log.Print(todos)
+	http.Redirect(w, r, "/todos", http.StatusSeeOther)
+}
+
 func main() {
 	http.HandleFunc("/", home)
 	http.HandleFunc("/todos", getTodos)
+	http.HandleFunc("/add-todo", AddTodo)
 	fmt.Println("Server on port :8080")
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
